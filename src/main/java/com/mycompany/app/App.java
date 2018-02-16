ba54940244f6e1aef664919d8b2bcaea2c3bcc1a
errenkoc <erenkoc95@gmail.com>
2018-02-16 20:59:36 +0300

    ended probably

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -13,13 +13,19 @@ import spark.template.mustache.MustacheTemplateEngine;
 
 public class App
 {
-    public static boolean search(ArrayList<Integer> array, int e) {
-      System.out.println("inside search");
-      if (array == null) return false;
+    public static boolean search(int[] arr1, int[] arr2, int i1, int i2) {
+      System.out.println("Search array 1");
+      if (arr1 == null || arr2 == null) return false;
 
-      for (int elt : array) {
-        if (elt == e) return true;
+      for (int elt : arr1) {
+        if (elt == (i1*i2)) {
+		 for (int elt2 : arr2)
+        		if (elt2 == (i1*i2)) return false;
+		return true;
+      	}
       }
+      for (int elt2 : arr2)
+       		if (elt2 == (i1*i2)) return true;
       return false;
     }
 
@@ -35,19 +41,34 @@ public class App
           String input1 = req.queryParams("input1");
           java.util.Scanner sc1 = new java.util.Scanner(input1);
           sc1.useDelimiter("[;\r\n]+");
-          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+          int[] arr1 = new int[input1.length()];
+	  int i= 0;
           while (sc1.hasNext())
           {
-            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
-            inputList.add(value);
+            int value1 = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+            arr1[i]= value1;
+	    i++;
           }
-          System.out.println(inputList);
+		
+	  String input2 = req.queryParams("input2");
+          java.util.Scanner sc2 = new java.util.Scanner(input2);
+          sc2.useDelimiter("[;\r\n]+");
+          int[] arr2 = new int[input2.length()];
+	  i= 0;
+          while (sc2.hasNext())
+          {
+            int value2 = Integer.parseInt(sc2.next().replaceAll("\\s",""));
+            arr2[i]= value2;
+	    i++;
+          }
 
+          String input3 = req.queryParams("input3").replaceAll("\\s","");
+          int int1 = Integer.parseInt(input3);
 
-          String input2 = req.queryParams("input2").replaceAll("\\s","");
-          int input2AsInt = Integer.parseInt(input2);
+          String input4 = req.queryParams("input4").replaceAll("\\s","");
+          int int2 = Integer.parseInt(input4);
 
-          boolean result = App.search(inputList, input2AsInt);
+          boolean result = App.search(arr1, arr2, int1, int2);
 
          Map map = new HashMap();
           map.put("result", result);
